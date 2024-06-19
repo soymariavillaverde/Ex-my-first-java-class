89b06fcb029c1ff44c790ff16f9b242086030a47
soymariavillaverde <soymariavillaverde@gmail.com>
2024-06-19 13:11:02 +0200

    Second part of the exercise solved :rocket:

--- creating-people/src/main/java/dev/maria/App.java
+++ creating-people/src/main/java/dev/maria/App.java
@@ -6,10 +6,10 @@ public final class App {
 
     public static void main(String[] args) {
         
-        Person person = new Person("Paquita", "Salas", "12345678Q", 1981);
+        Person person = new Person("Paquita", "Salas", "12345678Q", 1981, "Madrid", 'F');
         person.PrintAttributes();
 
-        Person otherPerson = new Person("Magüi", "Santa", "98765432A", 1990);
+        Person otherPerson = new Person("Magüi", "Santa", "98765432A", 1990, "Valencia", 'O');
         otherPerson.PrintAttributes();
         
     }
--- creating-people/src/main/java/dev/maria/Person.java
+++ creating-people/src/main/java/dev/maria/Person.java
@@ -6,13 +6,21 @@ public class Person {
     private String lastName;
     private String idNumber;
     private int birthYear;
+    private String originCountry;
+    private char gender;
 
 
-    public Person (String name, String lastName, String idNumber, int birthYear ){
+    public Person (String name, String lastName, String idNumber, int birthYear, String originCountry, char gender){
         this.name = name;
         this.lastName = lastName;
         this.idNumber = idNumber;
         this.birthYear = birthYear;
+        this.originCountry = originCountry;
+        if (gender == 'M' || gender == 'F' || gender == 'O' ) {
+            this.gender = gender;
+        } else {
+            throw new IllegalArgumentException("The gender entered is not valid. It must be 'M', 'F' or 'O'");
+        }
     };
 
 
@@ -48,12 +56,32 @@ public class Person {
         this.birthYear = birthYear;
     }
 
+    public String getOriginCountry() {
+        return originCountry;
+    }
+
+    public void setOriginCountry(String originCountry) {
+        this.originCountry = originCountry;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+
 
     public void PrintAttributes(){
         System.out.println("Name: " + name);
         System.out.println("Last name: " + lastName);
         System.out.println("ID number: " + idNumber);
         System.out.println("Day of birth: " + birthYear);
+        System.out.println("Country of origin: " + originCountry);
+        System.out.println("Gender: " + gender);
+
     }
 
 }
